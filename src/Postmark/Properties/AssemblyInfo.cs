f0bef8f883260d5b3dfb01b0003ee2af81d9491d
Dimebrain <daniel.crenna@gmail.com>
2010-03-28 00:56:27 -0400

    Changes to enable partial trust, and correct merge issue that made API classes internal

--- src/Postmark/Properties/AssemblyInfo.cs
+++ src/Postmark/Properties/AssemblyInfo.cs
@@ -55,6 +55,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -68,7 +69,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Lunarbits 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Postmark.Tests")]
+[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

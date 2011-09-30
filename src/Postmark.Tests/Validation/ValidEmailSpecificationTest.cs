d07f939a517781e73ec5c1f462e5cb8fb7bc91fd
Scott Moak <scott.moak@gmail.com>
2011-09-30 11:28:58 -0700

    Changed email validation regex to be more in line with RFC 2822 which should handle 99.9% of emails. Also added tests for this new regex to ensure it works as expected. Also removed reference to a non existant file in the test project.

---
+++ src/Postmark.Tests/Validation/ValidEmailSpecificationTest.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ValidEmailSpecificationTest.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Postmark.Tests.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    using PostmarkDotNet.Validation;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [TestFixture]
+    public class ValidEmailSpecificationTest
+    {
+        [TestCase("example@example.com")]
+        [TestCase("John Smith <john@johnsmith.com>")]
+        [TestCase("Jonh_Smith/SomeCoolDude/With%Some%Characters@someschool.EDU")]
+        [TestCase("firstname.lastname@somewhere.else.in.time")]
+        [TestCase("john@aol.com.nospam")]
+        [TestCase("john@server.department.company.com")]
+        public void IsSatisfiedBy_should_validate_emails_correctly(string email)
+        {
+            // Arrange
+            var emailSpecification = new ValidEmailSpecification();
+
+
+            // Act
+            var result = emailSpecification.IsSatisfiedBy(email);
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+    }
+}

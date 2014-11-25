f4a7f94a9511c776a1d6eeb370946598af12dab6
Andrew Theken <andrew@attr.io>
2014-11-25 15:49:33 -0500

    Requiring configuration variables to be set before running tests.

--- src/Postmark.PCL.Tests/ClientBaseFixture.cs
+++ src/Postmark.PCL.Tests/ClientBaseFixture.cs
@@ -9,6 +9,16 @@ namespace Postmark.PCL.Tests
     [TestFixture]
     public abstract class ClientBaseFixture
     {
+        private static void AssertSettingsAvailable()
+        {
+            Assert.NotNull(READ_INBOUND_TEST_SERVER_TOKEN, "READ_INBOUND_TEST_SERVER_TOKEN must be defined as an environment variable or app setting.");
+            Assert.NotNull(READ_SELENIUM_TEST_SERVER_TOKEN, "READ_SELENIUM_TEST_SERVER_TOKEN must be defined as an environment variable or app setting.");
+            Assert.NotNull(WRITE_ACCOUNT_TOKEN, "WRITE_ACCOUNT_TOKEN must be defined as an environment variable or app setting.");
+            Assert.NotNull(WRITE_TEST_SERVER_TOKEN, "WRITE_TEST_SERVER_TOKEN must be defined as an environment variable or app setting.");
+            Assert.NotNull(WRITE_TEST_SENDER_EMAIL_ADDRESS, "WRITE_TEST_SENDER_EMAIL_ADDRESS must be defined as an environment variable or app setting.");
+            Assert.NotNull(WRITE_TEST_EMAIL_RECIPIENT_ADDRESS, "WRITE_TEST_EMAIL_RECIPIENT_ADDRESS must be defined as an environment variable or app setting.");
+        }
+
         /// <summary>
         /// Retrieve the config variable from the environment, 
         /// or app.config if the environment doesn't specify it.
@@ -22,19 +32,20 @@ namespace Postmark.PCL.Tests
 
         public static readonly DateTime TESTING_DATE = DateTime.Now;
 
-        public static string READ_INBOUND_TEST_SERVER_TOKEN = ConfigVariable("READ_INBOUND_TEST_SERVER_TOKEN");
-        public static string READ_SELENIUM_TEST_SERVER_TOKEN = ConfigVariable("READ_SELENIUM_TEST_SERVER_TOKEN");
+        public static readonly string READ_INBOUND_TEST_SERVER_TOKEN = ConfigVariable("READ_INBOUND_TEST_SERVER_TOKEN");
+        public static readonly string READ_SELENIUM_TEST_SERVER_TOKEN = ConfigVariable("READ_SELENIUM_TEST_SERVER_TOKEN");
 
-        public static string WRITE_ACCOUNT_TOKEN = ConfigVariable("WRITE_ACCOUNT_TOKEN");
-        public static string WRITE_TEST_SERVER_TOKEN = ConfigVariable("WRITE_TEST_SERVER_TOKEN");
-        public static string WRITE_TEST_SENDER_EMAIL_ADDRESS = ConfigVariable("WRITE_TEST_SENDER_EMAIL_ADDRESS");
-        public static string WRITE_TEST_EMAIL_RECIPIENT_ADDRESS = ConfigVariable("WRITE_TEST_EMAIL_RECIPIENT_ADDRESS");
+        public static readonly string WRITE_ACCOUNT_TOKEN = ConfigVariable("WRITE_ACCOUNT_TOKEN");
+        public static readonly string WRITE_TEST_SERVER_TOKEN = ConfigVariable("WRITE_TEST_SERVER_TOKEN");
+        public static readonly string WRITE_TEST_SENDER_EMAIL_ADDRESS = ConfigVariable("WRITE_TEST_SENDER_EMAIL_ADDRESS");
+        public static readonly string WRITE_TEST_EMAIL_RECIPIENT_ADDRESS = ConfigVariable("WRITE_TEST_EMAIL_RECIPIENT_ADDRESS");
 
         protected PostmarkClient _client;
 
         [SetUp]
         public void RunSetupSynchronously()
         {
+            AssertSettingsAvailable();
             Setup();
         }
 

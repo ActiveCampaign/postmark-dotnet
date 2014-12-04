0b4c7b9bb9fdbea758263b152b850ae0aec49bdd
Andrew Theken <andrew@attr.io>
2014-12-04 14:31:45 -0500

    Updating Nuspec to include PCL and docs.

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,3 +1,2 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe src\PostmarkDotNet.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
-copy LICENSE bin
-NuGet.exe pack postmark.nuspec -Basepath bin
+NuGet.exe pack postmark.nuspec

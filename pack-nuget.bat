b04a0eda66d5582e0c90200636e010e49614b964
Andrew Theken <theken.1@osu.edu>
2015-01-22 13:54:32 -0500

    Fixing the nuget packaging script to use \ instead of /

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -6,17 +6,17 @@ mkdir PCL
 mkdir Convenience
 cd ..
 
-copy ./src/Postmark.PCL/bin/Release/*.* ./sn-build/PCL
-copy ./src/Postmark.Convenience/bin/Release/*.* ./sn-build/Convenience
+copy src\Postmark.PCL\bin\Release\*.* sn-build\PCL
+copy src\Postmark.Convenience\bin\Release\*.* sn-build\Convenience
 
-build-bin\ildasm.exe ./sn-build/PCL/Postmark.dll /out=pcl.il
-rm ./sn-build/PCL/Postmark.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe pcl.il /res:pcl.res /dll /key:src/Postmark.PCL/key.snk /out:sn-build\PCL\Postmark.dll
+build-bin\ildasm.exe sn-build\PCL\Postmark.dll /out=pcl.il
+rm sn-build\PCL\Postmark.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe pcl.il /res:pcl.res /dll /key:src\Postmark.PCL\key.snk /out:sn-build\PCL\Postmark.dll
 
 copy sn-build\PCL\Postmark.dll sn-build\Convenience\Postmark.dll
 
-build-bin\ildasm.exe ./sn-build/Convenience/Postmark.Convenience.dll /out=Convenience.il
+build-bin\ildasm.exe sn-build\Convenience\Postmark.Convenience.dll /out=Convenience.il
 rm ./sn-build/Convenience/Postmark.Convenience.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe Convenience.il /res:Convenience.res /dll /key:src/Postmark.PCL/key.snk /out:sn-build\Convenience\Postmark.Convenience.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe Convenience.il /res:Convenience.res /dll /key:src\Postmark.PCL\key.snk /out:sn-build\Convenience\Postmark.Convenience.dll
 
 NuGet.exe pack ./postmark-strong.nuspec

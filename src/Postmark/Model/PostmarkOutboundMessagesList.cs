1ec312a17d5451973d4fdc9cd4c91ac61953b330
JP Toto <james.p.toto@gmail.com>
2015-09-30 13:25:41 -0400

    Change to IEnumerable for the list of MessageEvents and Dictionary<string, string> for the event details

--- src/Postmark/Model/PostmarkOutboundMessagesList.cs
+++ src/Postmark/Model/PostmarkOutboundMessagesList.cs
@@ -42,7 +42,7 @@ namespace PostmarkDotNet.Model
         public string TextBody { get; set; }
         public string HtmlBody { get; set; }
         public string Body { get; set; }
-        public List<MessageEvent> MessageEvents { get; set; } 
+        public IEnumerable<MessageEvent> MessageEvents { get; set; } 
     }
 
     public class MessageEvent
@@ -50,7 +50,7 @@ namespace PostmarkDotNet.Model
         public string Recipient { get; set; }
         public string Type { get; set; }
         public DateTime ReceivedAt { get; set; }
-        public object Details { get; set; }
+        public Dictionary<string, string> Details { get; set; }
     }
 
     public class MessageDump

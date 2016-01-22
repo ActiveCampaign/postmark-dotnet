87d1abd9408f7baad64261a8637ebcab82272053
Andrew Theken <theken.1@osu.edu>
2016-01-22 10:05:27 -0500

    Add documentation for `TemplateModel`
    Partially addresses issue #34.

--- src/Postmark.PCL/Model/TemplatedPostmarkMessage.cs
+++ src/Postmark.PCL/Model/TemplatedPostmarkMessage.cs
@@ -24,6 +24,20 @@
 
         /// <summary>
         /// The values to merge with the template when creating the content.
+        ///
+        /// The message sending API requires that this be an *object* as described by the JSON specification.
+        ///
+        /// This means that you can assign a Dictionary&lt;K,V&gt; to this property. 
+        /// The dictionary may contain any keys and or objects that can be serialized to JSON.
+        /// 
+        /// Additionally, POCOs and anonymous types can be assign to this property, 
+        /// provided they can be serialized to JSON (we use JSON.net internally).
+        ///
+        /// Objects that would be serialized as "JSON scalars" or arrays should *not* be assigned to this property.
+        /// (They MAY be set as values on the TemplateModel, though)
+        /// 
+        /// See this guide for more information on how this model is used, and how Postmark Templates work:
+        /// http://support.postmarkapp.com/article/786-using-a-postmark-starter-template
         /// </summary>
         public object TemplateModel { get; set; }
     }

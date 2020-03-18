8c5960a37e7a2df9b18f560974235a43e1c7b7f0
Vlad Sandu <vladsandu1994@gmail.com>
2020-03-18 09:34:12 +0200

    Refactor PostmarkClient.cs

--- src/Postmark/PostmarkClient.cs
+++ src/Postmark/PostmarkClient.cs
@@ -858,40 +858,40 @@ namespace PostmarkDotNet
 
         public async Task<PostmarkResponse> SendEmailWithTemplateAsync<T>(string templateAlias, T templateModel,
             string to, string from,
-            string messageStream = null,
             bool? inlineCss = null, string cc = null,
             string bcc = null, string replyTo = null,
             bool? trackOpens = null,
             IDictionary<string, string> headers = null,
             IDictionary<string, string> metadata = null,
+            string messageStream = null,
             params PostmarkMessageAttachment[] attachments)
         {
-            return await InternalSendEmailWithTemplateAsync(templateAlias, templateModel, messageStream, to, from, inlineCss, cc,
-            bcc, replyTo, trackOpens, headers, metadata, attachments);
+            return await InternalSendEmailWithTemplateAsync(templateAlias, templateModel, to, from, inlineCss, cc,
+            bcc, replyTo, trackOpens, headers, metadata, messageStream, attachments);
         }
 
         public async Task<PostmarkResponse> SendEmailWithTemplateAsync<T>(long templateId, T templateModel,
             string to, string from,
-            string messageStream = null,
             bool? inlineCss = null, string cc = null,
             string bcc = null, string replyTo = null,
             bool? trackOpens = null,
             IDictionary<string, string> headers = null,
             IDictionary<string, string> metadata = null,
+            string messageStream = null,
             params PostmarkMessageAttachment[] attachments)
         {
-            return await InternalSendEmailWithTemplateAsync(templateId, templateModel, messageStream, to, from, inlineCss, cc,
-            bcc, replyTo, trackOpens, headers, metadata, attachments);
+            return await InternalSendEmailWithTemplateAsync(templateId, templateModel, to, from, inlineCss, cc,
+            bcc, replyTo, trackOpens, headers, metadata, messageStream, attachments);
         }
 
         private async Task<PostmarkResponse> InternalSendEmailWithTemplateAsync<T>(object templateReference, T templateModel,
-            string messageStream,
             string to, string from,
             bool? inlineCss = null, string cc = null,
             string bcc = null, string replyTo = null,
             bool? trackOpens = null,
             IDictionary<string, string> headers = null,
             IDictionary<string, string> metadata = null,
+            string messageStream = null,
             params PostmarkMessageAttachment[] attachments)
         {
 
@@ -905,7 +905,7 @@ namespace PostmarkDotNet
                 email.TemplateAlias = (string)templateReference;
             }
             email.TemplateModel = templateModel;
-            email.MessageStream = messageStream;
+            email.MessageStream = messageStream ?? PostmarkMessageBase.DefaultTransactionalStream;
             email.To = to;
             email.From = from;
             if (inlineCss.HasValue)

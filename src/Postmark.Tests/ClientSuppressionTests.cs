ab6f2027e4d8b05c9765711bf7106d2837a456d0
Vlad Sandu <vladsandu1994@gmail.com>
2020-03-22 11:44:34 +0200

    Fix broken unit tests in ClientSuppressionTests

--- src/Postmark.Tests/ClientSuppressionTests.cs
+++ src/Postmark.Tests/ClientSuppressionTests.cs
@@ -17,7 +17,7 @@ namespace Postmark.Tests
         protected override void Setup()
         {
             _adminClient = new PostmarkAdminClient(WRITE_ACCOUNT_TOKEN, BASE_URL);
-            _server = _adminClient.CreateServerAsync($"integration-test-suppressions-{Guid.NewGuid()}").Result;
+            _server = MakeSynchronous(() => _adminClient.CreateServerAsync($"integration-test-suppressions-{Guid.NewGuid()}"));
             _client = new PostmarkClient(_server.ApiTokens.First(), BASE_URL);
         }
 
@@ -63,12 +63,86 @@ namespace Postmark.Tests
             Assert.Null(suppressionResult.Message);
         }
 
+        [Fact]
+        public async Task ClientCanListSuppressions()
+        {
+            var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
+
+            for (var i = 0; i < 5; i++)
+            {
+                var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+                suppressionRequests.Add(reactivationRequest);
+            }
+
+            var suppressionResult = await _client.CreateSuppressions(suppressionRequests);
+            Assert.Equal(5, suppressionResult.Suppressions.Count());
+            Assert.True(suppressionResult.Suppressions.All(k => k.Status == PostmarkSuppressionRequestStatus.Suppressed));
+
+            // Suppressions are being processed asynchronously so we must give it some time to process those requests
+            var suppressionListing = await PollUntil(() => _client.ListSuppressions(new PostmarkSuppressionQuery()),
+                k => k.Suppressions.Count() == 5);
+
+            Assert.Equal(5, suppressionListing.Suppressions.Count());
+        }
+
+        [Fact]
+        public async Task ClientCanFilterSuppressionsByEmail()
+        {
+            var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
+
+            for (var i = 0; i < 3; i++)
+            {
+                var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+                suppressionRequests.Add(reactivationRequest);
+            }
+
+            await _client.CreateSuppressions(suppressionRequests);
+
+            var query = new PostmarkSuppressionQuery
+            {
+                EmailAddress = suppressionRequests.First().EmailAddress
+            };
+
+            // Suppressions are being processed asynchronously so we must give it some time to process those requests
+
+            var suppressionListing = await PollUntil(() => _client.ListSuppressions(query), k => k.Suppressions.Count() == 1);
+
+            Assert.Single(suppressionListing.Suppressions);
+
+            var actualSuppression = suppressionListing.Suppressions.First();
+
+            Assert.Equal(suppressionRequests.First().EmailAddress, actualSuppression.EmailAddress);
+            Assert.Equal("Customer", actualSuppression.Origin);
+            Assert.Equal("ManualSuppression", actualSuppression.SuppressionReason);
+        }
+
+        private async Task<T> PollUntil<T>(Func<Task<T>> pollingTaskFunc, Func<T, bool> isComplete, int retriesLeft = 5, int delayInMs = 1000)
+        {
+            var result = await pollingTaskFunc();
+
+            if (isComplete(result) || retriesLeft == 0)
+            {
+                return result;
+            }
+
+            await Task.Delay(delayInMs).ConfigureAwait(false);
+
+            return await PollUntil(pollingTaskFunc, isComplete, --retriesLeft);
+        }
+
         private Task Cleanup()
         {
             return Task.Run(async () =>
             {
                 await _adminClient.DeleteServerAsync(_server.ID);
             });
+        }
+
+        private T MakeSynchronous<T>(Func<Task<T>> t)
+        {
+            var f = Task.Run(async () => await t().ConfigureAwait(false));
+            f.Wait();
+            return f.Result;
         }
 
         public void Dispose()

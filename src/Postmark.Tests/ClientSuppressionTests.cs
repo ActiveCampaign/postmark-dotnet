eb4ed64c322c78980628cde98a595abb95629ca0
Vlad Sandu <vladsandu1994@gmail.com>
2020-03-22 10:34:20 +0200

    Uncomment Suppressions tests

--- src/Postmark.Tests/ClientSuppressionTests.cs
+++ src/Postmark.Tests/ClientSuppressionTests.cs
@@ -21,99 +21,98 @@ namespace Postmark.Tests
             _client = new PostmarkClient(_server.ApiTokens.First(), BASE_URL);
         }
 
-        //[Fact]
-        //public async void ClientCanSuppressRecipients()
-        //{
-        //    var suppressionRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
-        //    var result = await _client.CreateSuppressions(new List<PostmarkSuppressionChangeRequest> { suppressionRequest });
+        [Fact]
+        public async Task ClientCanSuppressRecipients()
+        {
+            var suppressionRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+            var result = await _client.CreateSuppressions(new List<PostmarkSuppressionChangeRequest> { suppressionRequest });
 
-        //    Assert.Single(result.Suppressions);
+            Assert.Single(result.Suppressions);
 
-        //    var suppressionResult = result.Suppressions.First();
-        //    Assert.Equal(suppressionRequest.EmailAddress, suppressionResult.EmailAddress);
-        //    Assert.Equal(PostmarkSuppressionRequestStatus.Suppressed, suppressionResult.Status);
-        //    Assert.Null(suppressionResult.Message);
-        //}
+            var suppressionResult = result.Suppressions.First();
+            Assert.Equal(suppressionRequest.EmailAddress, suppressionResult.EmailAddress);
+            Assert.Equal(PostmarkSuppressionRequestStatus.Suppressed, suppressionResult.Status);
+            Assert.Null(suppressionResult.Message);
+        }
 
-        //[Fact]
-        //public async void InvalidRequest_HasFailedStatus()
-        //{
-        //    var suppressionRequest = new PostmarkSuppressionChangeRequest { EmailAddress = "not-a-correct-email-address" };
-        //    var result = await _client.CreateSuppressions(new List<PostmarkSuppressionChangeRequest> { suppressionRequest });
+        [Fact]
+        public async Task InvalidRequest_HasFailedStatus()
+        {
+            var suppressionRequest = new PostmarkSuppressionChangeRequest { EmailAddress = "not-a-correct-email-address" };
+            var result = await _client.CreateSuppressions(new List<PostmarkSuppressionChangeRequest> { suppressionRequest });
 
-        //    Assert.Single(result.Suppressions);
+            Assert.Single(result.Suppressions);
 
-        //    var suppressionResult = result.Suppressions.First();
-        //    Assert.Equal(suppressionRequest.EmailAddress, suppressionResult.EmailAddress);
-        //    Assert.Equal(PostmarkSuppressionRequestStatus.Failed, suppressionResult.Status);
-        //    Assert.NotNull(suppressionResult.Message);
-        //}
+            var suppressionResult = result.Suppressions.First();
+            Assert.Equal(suppressionRequest.EmailAddress, suppressionResult.EmailAddress);
+            Assert.Equal(PostmarkSuppressionRequestStatus.Failed, suppressionResult.Status);
+            Assert.NotNull(suppressionResult.Message);
+        }
 
-        //[Fact]
-        //public async void ClientCanDeleteSuppressions()
-        //{
-        //    var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
-        //    var result = await _client.DeleteSuppressions(new List<PostmarkSuppressionChangeRequest> { reactivationRequest });
+        [Fact]
+        public async Task ClientCanDeleteSuppressions()
+        {
+            var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+            var result = await _client.DeleteSuppressions(new List<PostmarkSuppressionChangeRequest> { reactivationRequest });
 
-        //    Assert.Single(result.Suppressions);
+            Assert.Single(result.Suppressions);
 
-        //    var suppressionResult = result.Suppressions.First();
-        //    Assert.Equal(reactivationRequest.EmailAddress, suppressionResult.EmailAddress);
-        //    Assert.Equal(PostmarkSuppressionRequestStatus.Deleted, suppressionResult.Status);
-        //    Assert.Null(suppressionResult.Message);
-        //}
+            var suppressionResult = result.Suppressions.First();
+            Assert.Equal(reactivationRequest.EmailAddress, suppressionResult.EmailAddress);
+            Assert.Equal(PostmarkSuppressionRequestStatus.Deleted, suppressionResult.Status);
+            Assert.Null(suppressionResult.Message);
+        }
 
-        //[Fact]
-        //public async void ClientCanListSuppressions()
-        //{
-        //    var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
+        [Fact]
+        public async Task ClientCanListSuppressions()
+        {
+            var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
 
-        //    for (var i = 0; i < 5; i++)
-        //    {
-        //        var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
-        //        suppressionRequests.Add(reactivationRequest);
-        //    }
+            for (var i = 0; i < 5; i++)
+            {
+                var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+                suppressionRequests.Add(reactivationRequest);
+            }
 
-        //    var suppressionResult = await _client.CreateSuppressions(suppressionRequests);
-        //    Assert.Equal(5, suppressionResult.Suppressions.Count());
-        //    Assert.True(suppressionResult.Suppressions.All(k => k.Status == PostmarkSuppressionRequestStatus.Suppressed));
+            var suppressionResult = await _client.CreateSuppressions(suppressionRequests);
+            Assert.Equal(5, suppressionResult.Suppressions.Count());
+            Assert.True(suppressionResult.Suppressions.All(k => k.Status == PostmarkSuppressionRequestStatus.Suppressed));
 
-        //    // Suppressions are being processed asynchronously so we must give it some time to process those requests
-        //    await Task.Delay(5000);
+            // Suppressions are being processed asynchronously so we must give it some time to process those requests
+            await Task.Delay(5000);
 
-        //    var suppressionListing = await _client.ListSuppressions(new PostmarkSuppressionQuery());
-        //    Assert.Equal(5, suppressionListing.Suppressions.Count());
-        //}
+            var suppressionListing = await _client.ListSuppressions(new PostmarkSuppressionQuery());
+            Assert.Equal(5, suppressionListing.Suppressions.Count());
+        }
 
+        [Fact]
+        public async Task ClientCanFilterSuppressionsByEmail()
+        {
+            var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
 
-        //[Fact]
-        //public async void ClientCanFilterSuppressionsByEmail()
-        //{
-        //    var suppressionRequests = new List<PostmarkSuppressionChangeRequest>();
+            for (var i = 0; i < 3; i++)
+            {
+                var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
+                suppressionRequests.Add(reactivationRequest);
+            }
 
-        //    for (var i = 0; i < 3; i++)
-        //    {
-        //        var reactivationRequest = new PostmarkSuppressionChangeRequest { EmailAddress = $"test-{Guid.NewGuid().ToString()}@gmail.com" };
-        //        suppressionRequests.Add(reactivationRequest);
-        //    }
+            await _client.CreateSuppressions(suppressionRequests);
 
-        //    await _client.CreateSuppressions(suppressionRequests);
+            // Suppressions are being processed asynchronously so we must give it some time to process those requests
+            await Task.Delay(5000);
 
-        //    // Suppressions are being processed asynchronously so we must give it some time to process those requests
-        //    await Task.Delay(5000);
+            var suppressionListing = await _client.ListSuppressions(new PostmarkSuppressionQuery
+            {
+                EmailAddress = suppressionRequests.First().EmailAddress
+            });
+            Assert.Single(suppressionListing.Suppressions);
 
-        //    var suppressionListing = await _client.ListSuppressions(new PostmarkSuppressionQuery
-        //    {
-        //        EmailAddress = suppressionRequests.First().EmailAddress
-        //    });
-        //    Assert.Single(suppressionListing.Suppressions);
+            var actualSuppression = suppressionListing.Suppressions.First();
 
-        //    var actualSuppression = suppressionListing.Suppressions.First();
-
-        //    Assert.Equal(suppressionRequests.First().EmailAddress, actualSuppression.EmailAddress);
-        //    Assert.Equal("Customer", actualSuppression.Origin);
-        //    Assert.Equal("ManualSuppression", actualSuppression.SuppressionReason);
-        //}
+            Assert.Equal(suppressionRequests.First().EmailAddress, actualSuppression.EmailAddress);
+            Assert.Equal("Customer", actualSuppression.Origin);
+            Assert.Equal("ManualSuppression", actualSuppression.SuppressionReason);
+        }
 
         private Task Cleanup()
         {

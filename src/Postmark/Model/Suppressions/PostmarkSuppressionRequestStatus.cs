7a08bed95f0585e006eb21fd7e1569c7f716f853
Vlad Sandu <vladsandu1994@gmail.com>
2020-04-01 16:34:06 +0300

    Update PostmarkSuppressionRequestStatus documentation

--- src/Postmark/Model/Suppressions/PostmarkSuppressionRequestStatus.cs
+++ src/Postmark/Model/Suppressions/PostmarkSuppressionRequestStatus.cs
@@ -5,6 +5,17 @@
     /// </summary>
     public enum PostmarkSuppressionRequestStatus
     {
-        Suppressed, Failed, Deleted
+        /// <summary>
+        /// We have received the request to Suppress the recipient.
+        /// </summary>
+        Suppressed,
+        /// <summary>
+        /// We have received the request to reactivate the recipient.
+        /// </summary>
+        Deleted,
+        /// <summary>
+        /// Request submission has failed. Please see the result Message field for information.
+        /// </summary>
+        Failed
     }
 }

d625c4878775036f4799d0a6970c53e5b91b9ab8
R4cOOn <1311219+R4cOOn@users.noreply.github.com>
2022-04-13 16:08:06 +0200

    Add custom exception.

---
+++ src/Postmark/Exceptions/PostmarkResponseException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Postmark.Exceptions
+{
+    [Serializable]
+    public class PostmarkResponseException : Exception
+    {
+        public string Body { get; private set; }
+
+        public PostmarkResponseException(string body)
+        {
+            Body = body;
+        }
+
+        public PostmarkResponseException(string message, string body) : base(message)
+        {
+            Body = body;
+        }
+
+        public PostmarkResponseException(string message, string body, Exception inner) : base(message, inner)
+        {
+            Body = body;
+        }
+
+        protected PostmarkResponseException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
